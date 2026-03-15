7be8d0e22ac5a8a2c567da933eb66b7155343107
susanakiku-collab <susana.kiku@gmail.com>
2026-03-15 09:53:08 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+window.APP_CONFIG = {
+
+  SUPABASE_URL: "https://cgtkbroxqdamnirjhzxy.supabase.co",
+
+  SUPABASE_ANON_KEY: "sb_publishable_veQ6nay4yBjAHz95Hv55ng_QahlWYUz",
+
+  // THEMIS
+  ORIGIN_LABEL: "THEMIS",
+  ORIGIN_LAT: 35.784865651108156,
+  ORIGIN_LNG:  139.9003012359358,
+
+  GOOGLE_MAPS_API_KEY: "AIzaSyCHp8CAK-Z1OXWpDzill27MKLzRSNoZ5hI"
+};
+ 
